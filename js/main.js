667922fac84ad90dc600cde8d0b3cac51243a6af
Mikele91 <michele.amoruso91.8@gmail.com>
2021-09-13 11:21:19 +0200

    aiuto

---
+++ js/main.js
@@ -0,0 +1,120 @@
+const icons = [
+	{
+        name: 'apple-alt',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "food"
+	},
+	{
+        name: 'ice-cream',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "food"
+	},
+	{
+        name: 'fish',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "food"
+	},
+	{
+        name: 'lemon',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "food"
+	},
+	{
+        name: 'hamburger',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "food"
+	},
+	{
+        name: 'pizza-slice',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "food"
+	},
+	{
+        name: 'beer',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "beverage"
+	},
+	{
+        name: 'glass-whiskey',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "beverage"
+	},
+	{
+        name: 'wine-bottle',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "beverage"
+	},
+	{
+        name: 'cocktail',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "beverage"
+	},
+	{
+        name: 'coffee',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "beverage"
+	},
+	{
+        name: 'glass-martini',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "beverage"
+	},
+	{
+        name: 'dragon',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "animal"
+	},
+	{
+        name: 'kiwi-bird',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "animal"
+	},
+	{
+        name: 'frog',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "animal"
+	},
+	{
+        name: 'hippo',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "animal"
+	},
+	{
+        name: 'otter',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "animal"
+	},
+	{
+        name: 'horse',
+        family: 'fas',
+        prefix: 'fa-',
+        category: "animal"
+	},
+];
+// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+icons.forEach((elem) =>{
+    console.log(`<div id="card" class="card">
+    <i class="${elem.family}  ${elem.prefix}${elem.name}"></i>
+    <div id="nome_card" class="nome_card">
+        ${elem.name}
+    </div>
+</div>`);
+}
+);
