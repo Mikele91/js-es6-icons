e6b184eadab3b6a39d5cf4029d27b829e64ad2a1
Mikele91 <michele.amoruso91.8@gmail.com>
2021-09-13 16:07:16 +0200

    push 2

--- js/main.js
+++ js/main.js
@@ -1,120 +1,97 @@
-const icons = [
-	{
-        name: 'apple-alt',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "food"
-	},
-	{
-        name: 'ice-cream',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "food"
-	},
-	{
-        name: 'fish',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "food"
-	},
-	{
-        name: 'lemon',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "food"
-	},
-	{
-        name: 'hamburger',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "food"
-	},
-	{
-        name: 'pizza-slice',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "food"
-	},
-	{
-        name: 'beer',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "beverage"
-	},
-	{
-        name: 'glass-whiskey',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "beverage"
-	},
-	{
-        name: 'wine-bottle',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "beverage"
-	},
-	{
-        name: 'cocktail',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "beverage"
-	},
-	{
-        name: 'coffee',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "beverage"
-	},
-	{
-        name: 'glass-martini',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "beverage"
-	},
-	{
-        name: 'dragon',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "animal"
-	},
-	{
-        name: 'kiwi-bird',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "animal"
-	},
-	{
-        name: 'frog',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "animal"
-	},
-	{
-        name: 'hippo',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "animal"
-	},
-	{
-        name: 'otter',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "animal"
-	},
-	{
-        name: 'horse',
-        family: 'fas',
-        prefix: 'fa-',
-        category: "animal"
-	},
-];
+
+
+const colors = {
+    food : "pink",
+    animal: "green",
+    beverage:"yellow"
+}
+const containerIcons = document.getElementById("container"); 
+//argomenti l'array icone e il nodo html dove stampare le icone
+const printIcons = (arr, container) => {
+    container.innerHTML = "";
+    arr.forEach((elem) =>{
+        // destrutturazione 
+        // const{name, family, prefix} = elem; Non mi funziona 
+        container.innerHTML +=(`<div class="card">
+        <i class="${elem.family}  ${elem.prefix}${elem.name} style = "color: color"></i>
+        <div id="nome_card" class="nome_card">
+            ${elem.name}
+        </div>
+    </div>`);
+    }                         
+    );
+
+}
+const iconsColors = icons.map(
+    (elem) => {
+        console.log(elem.category);
+        return{
+            ...elem,
+            // name: elem.name,
+            // family: elem.family,
+            // prefix: elem.prefix,
+            // category: elem.category,
+
+            color: colors[elem.category]//aggiunta una proprietà!
+        };
+    
+    }
+);
+
+
+printIcons (iconsColors, containerIcons)
 // Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
-icons.forEach((elem) =>{
-    document.getElementById("container").innerHTML +=(`<div id="card" class="card">
-    <i class="${elem.family}  ${elem.prefix}${elem.name}"></i>
-    <div id="nome_card" class="nome_card">
-        ${elem.name}
-    </div>
-</div>`);
-}                         
+// icons.forEach((elem) =>{
+    // destrutturazione 
+//      const{name, family, prefix} = elem; 
+//     document.getElementById("container").innerHTML +=(`<div class="card">
+//     <i class="${elem.family}  ${elem.prefix}${elem.name}"></i>
+//     <div id="nome_card" class="nome_card">
+//         ${elem.name}
+//      </div>
+//  </div>`);
+// // }                         
+// );
+
+
+
+
+
+const iconCategories =[];
+icons.forEach(
+    (elem)=> {
+        if( iconCategories.includes(elem.category) ==false ){
+
+            iconCategories.push(elem.category);
+        }
+    }
+)
+console.log(iconCategories);
+
+
+
+// creo una option per ogni categoria 
+
+const selectCategory =document.getElementById("category");
+
+iconCategories.forEach(
+    (elem)=> {
+        selectCategory.innerHTML += `<option value="${elem}">${elem}</option>`
+    }
+);
+
+// creo evento sulla select
+selectCategory.addEventListener("change",
+()=>{
+    // recupero valore select 
+    const iconsFilter= iconsColors.filter(
+        (elem) => {
+            if (elem.category == selectCategory.value || selectCategory.value ==""){
+                return true;
+            }
+            return false;
+        }
+    )
+    printIcons (iconsFilter, containerIcons);
+}
 );
